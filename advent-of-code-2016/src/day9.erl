06b4a48f1ac81fa38d92bc39ab8427749f2e58cd
haljin <paul.shinobi@gmail.com>
2016-12-09 16:32:43 +0100

    Day 9 finished

--- advent-of-code-2016/src/day9.erl
+++ advent-of-code-2016/src/day9.erl
@@ -2,10 +2,6 @@
 
 -compile(export_all).
 
--record(marker, {length,
-                 reptitions,
-                 sub_markers}).
-
 parse_data(File) ->
   {ok, Fd} = file:open(File, [read]),
   {ok, Line} = file:read_line(Fd),
@@ -15,6 +11,10 @@ solve() ->
   Data = parse_data("../priv/day9input.txt"),
   Decompressed = decompress(Data),
   length(Decompressed).
+
+solve2() ->
+  Data = parse_data("../priv/day9input.txt"),
+  decompress2(Data).
 
 decompress(Data) ->
   clear(Data, []).
@@ -42,3 +42,46 @@ decompressing(String, 0, Repetitions, Sequence, Full) when Repetitions > 0 ->
   decompressing(String, 0, Repetitions - 1, Sequence, Sequence ++ Full);
 decompressing(String, 0, 0, _Sequence, Full) ->
   clear(String, Full).
+
+decompress2(Data) ->
+  MainWorker = spawn(?MODULE, init_worker, [Data, self(), [], 0]),
+  receive
+    {sum, MainWorker, Sum} ->
+      Sum
+  end.
+%%init_worker(Data, self(), [], 0).
+
+init_worker(Data, ParentPid, ChildPids, Sum) ->
+  clear2(Data, ParentPid, ChildPids, Sum).
+
+clear2([$( | Rest], ParentPid, ChildPids, Acc) ->
+  start_marker2(Rest, ParentPid, ChildPids, [], Acc);
+clear2([_Char | Rest], ParentPid, ChildPids, Acc) ->
+  clear2(Rest, ParentPid, ChildPids, Acc + 1);
+clear2([], ParentPid, [], Acc) ->
+  ParentPid ! {sum, self(), Acc};
+%%  Acc;
+clear2([], ParentPid, ChildPids, Acc) ->
+  receive
+    {sum, Pid, SubSum} ->
+      {Pid, Multiplicator} = lists:keyfind(Pid, 1, ChildPids),
+      clear2([], ParentPid, lists:keydelete(Pid, 1, ChildPids), Acc + Multiplicator * SubSum)
+  end.
+
+start_marker2([$x | Rest], ParentPid, ChildPids, Length, Acc) ->
+  mid_marker2(Rest, ParentPid, ChildPids, list_to_integer(Length), [], Acc);
+start_marker2([Char | Rest], ParentPid, ChildPids, Length, Acc) ->
+  start_marker2(Rest, ParentPid, ChildPids, Length ++ [Char], Acc).
+
+mid_marker2([$) | Rest], ParentPid, ChildPids, Length, Repetitions, Acc) ->
+  sequence2(Rest, ParentPid, ChildPids, Length, list_to_integer(Repetitions), [], Acc);
+mid_marker2([Char | Rest], ParentPid, ChildPids, Length, Repetitions, Acc) ->
+  mid_marker2(Rest, ParentPid, ChildPids, Length, Repetitions ++ [Char], Acc).
+
+sequence2([Char | Rest], ParentPid, ChildPids, Length, Repetitions, Sequence, Acc) when length(Sequence) < Length ->
+  sequence2(Rest, ParentPid, ChildPids, Length, Repetitions, [Char | Sequence], Acc);
+sequence2(FullString, ParentPid, ChildPids, _Length, Repetitions, Sequence, Acc) ->
+  NewChild = spawn(?MODULE, init_worker, [lists:reverse(Sequence), self(), [], 0]),
+  clear2(FullString, ParentPid, [{NewChild, Repetitions} | ChildPids], Acc).
+%%  SubSum = Repetitions * clear2(lists:reverse(Sequence), self(), [], 0),
+%%  clear2(FullString, ParentPid, ChildPids, Acc + SubSum).
