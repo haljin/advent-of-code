daee652452930672ae2e660173d7523cfa4732d4
Pawel Antemijczuk <pan@issuu.com>
2017-12-03 13:16:49 +0100

    Fun with closures

--- advent_of_code_2017/lib/day_3.ex
+++ advent_of_code_2017/lib/day_3.ex
@@ -20,7 +20,7 @@ defmodule AdventOfCode2017.Day3 do
       31
   """
   def solve(input) do
-    build_spiral(:left, {0, 0}, {1, -1, 1, -1}, &path_fun/3, {1, input})
+    build_spiral(:left, {0, 0}, {1, -1, 1, -1}, build_path_fun(1, input)) #&path_fun/3, {1, input})
   end
 
   @doc """
@@ -40,60 +40,67 @@ defmodule AdventOfCode2017.Day3 do
       304
   """
   def solve2(input) do
-    build_spiral(:left, {0, 0}, {1, -1, 1, -1}, &neighbour_sum_fum/3, {%{{0,0} => 1}, input})
+    build_spiral(:left, {0, 0}, {1, -1, 1, -1}, build_neighbour_sum_fun(%{{0,0} => 1}, input))
   end
 
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
-  defp build_spiral(dir, {curX, curY}, boundaries, validateFun, data) do
-    case validateFun.(curX, curY, data) do
+  defp build_spiral(dir, {curX, curY}, boundaries, validateFun) do
+    case validateFun.(curX, curY) do
       {:done, val} -> val
-      newData -> make_spiral(dir, {curX, curY}, boundaries, validateFun, newData)
+      newValidateFun -> make_spiral(dir, {curX, curY}, boundaries, newValidateFun)
     end
   end
 
-  defp make_spiral(:left, {maxX, curY}, {maxX, minX, maxY, minY}, validateFun, data) do
-    build_spiral(:up, {maxX, curY + 1}, {maxX + 1, minX, maxY, minY}, validateFun, data)
+  defp make_spiral(:left, {maxX, curY}, {maxX, minX, maxY, minY}, validateFun) do
+    build_spiral(:up, {maxX, curY + 1}, {maxX + 1, minX, maxY, minY}, validateFun)
   end
-  defp make_spiral(:left, {curX, curY}, boundaries, validateFun, data) do
-    build_spiral(:left, {curX + 1, curY}, boundaries, validateFun, data)    
+  defp make_spiral(:left, {curX, curY}, boundaries, validateFun) do
+    build_spiral(:left, {curX + 1, curY}, boundaries, validateFun)    
   end
-  defp make_spiral(:up, {curX, maxY}, {maxX, minX, maxY, minY}, validateFun, data) do
-    build_spiral(:right, {curX - 1, maxY}, {maxX, minX, maxY + 1, minY}, validateFun, data)
+  defp make_spiral(:up, {curX, maxY}, {maxX, minX, maxY, minY}, validateFun) do
+    build_spiral(:right, {curX - 1, maxY}, {maxX, minX, maxY + 1, minY}, validateFun)
   end
-  defp make_spiral(:up, {curX, curY}, boundaries, validateFun, data) do
-    build_spiral(:up, {curX, curY + 1}, boundaries, validateFun, data)
+  defp make_spiral(:up, {curX, curY}, boundaries, validateFun) do
+    build_spiral(:up, {curX, curY + 1}, boundaries, validateFun)
   end
-  defp make_spiral(:right , {minX, curY}, {maxX, minX, maxY, minY}, validateFun, data) do
-    build_spiral(:down, {minX, curY - 1}, {maxX, minX - 1, maxY, minY}, validateFun, data) 
+  defp make_spiral(:right , {minX, curY}, {maxX, minX, maxY, minY}, validateFun) do
+    build_spiral(:down, {minX, curY - 1}, {maxX, minX - 1, maxY, minY}, validateFun) 
   end
-  defp make_spiral(:right, {curX, curY}, boundaries, validateFun, data) do
-    build_spiral(:right, {curX - 1, curY}, boundaries, validateFun, data)
+  defp make_spiral(:right, {curX, curY}, boundaries, validateFun) do
+    build_spiral(:right, {curX - 1, curY}, boundaries, validateFun)
   end
-  defp make_spiral(:down, {curX, minY}, {maxX, minX, maxY, minY}, validateFun, data) do
-    build_spiral(:left, {curX + 1, minY}, {maxX, minX, maxY, minY - 1}, validateFun, data) 
+  defp make_spiral(:down, {curX, minY}, {maxX, minX, maxY, minY}, validateFun) do
+    build_spiral(:left, {curX + 1, minY}, {maxX, minX, maxY, minY - 1}, validateFun) 
   end
-  defp make_spiral(:down, {curX, curY}, boundaries, validateFun, data) do
-    build_spiral(:down, {curX, curY - 1}, boundaries, validateFun, data)
-  end
-
-  defp path_fun(curX, curY, {finalN, finalN}) do
-    {:done, abs(curX) + abs(curY)}
-  end
-  defp path_fun(_, _, {n, finalN}) do
-    {n + 1, finalN}
+  defp make_spiral(:down, {curX, curY}, boundaries, validateFun) do
+    build_spiral(:down, {curX, curY - 1}, boundaries, validateFun)
   end
 
-  defp neighbour_sum_fum(curX, curY, {spiralMap, solution}) do
-    mySum =
-    (for x <- (curX - 1)..(curX + 1), y <- (curY - 1)..(curY + 1), do: spiralMap[{x, y}]) |>
-    Enum.filter(&(&1 != nil)) |>
-    Enum.sum
-    if mySum > solution do
-      {:done, mySum}
-    else
-      {Map.put(spiralMap, {curX, curY}, mySum), solution}
+  defp build_path_fun(finalN, finalN) do
+    fn (curX, curY) ->
+        {:done, abs(curX) + abs(curY)}
     end
   end
+  defp build_path_fun(n, finalN) do
+    fn (_, _) ->
+      build_path_fun(n + 1, finalN)    
+    end
+  end
+
+  defp build_neighbour_sum_fun(spiralMap, solution) do
+    fn(curX, curY) ->
+      mySum =
+      (for x <- (curX - 1)..(curX + 1), y <- (curY - 1)..(curY + 1), do: spiralMap[{x, y}]) |>
+      Enum.filter(&(&1 != nil)) |>
+      Enum.sum
+      if mySum > solution do
+        {:done, mySum}
+      else
+        build_neighbour_sum_fun(Map.put(spiralMap, {curX, curY}, mySum), solution)
+      end    
+    end
+  end
+  
 end
