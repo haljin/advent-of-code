2acf6402d8e10292de84fe42b7c686aa7972d9fd
Pawel Antemijczuk <pan@issuu.com>
2017-12-05 10:51:24 +0100

    Moved data to separate files, solve day 5

---
+++ advent_of_code_2017/lib/day_5.ex
@@ -0,0 +1,71 @@
+defmodule AdventOfCode2017.Day5 do
+  @moduledoc """
+  Day 5 solutions
+  """
+
+  @doc """
+  Solves the first riddle of day 5.
+  Examples:
+
+      iex> AdventOfCode2017.Day5.solve("
+      ...> 0
+      ...> 3
+      ...> 0
+      ...> 1
+      ...> -3
+      ...> ")
+      5
+
+  """
+  def solve(input) do
+    parse_data(input)
+    |> move_on_list(1, 0)
+  end
+
+  @doc """
+  Solves the second riddle of day 5. 
+  Examples:
+
+      iex> AdventOfCode2017.Day5.solve2("
+      ...> 0
+      ...> 3
+      ...> 0
+      ...> 1
+      ...> -3
+      ...> ")
+      10
+
+  """
+  def solve2(input) do
+    parse_data(input)
+    |> move_on_list2(1, 0)    
+  end
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+  defp parse_data(input) do
+    numbers = 
+    input
+    |> String.split
+    |> Enum.map(&String.to_integer/1)
+    Enum.zip(1..length(numbers), numbers)
+    |> Enum.into(%{})
+  end
+  
+  defp move_on_list(map, position, step) do
+    case map[position] do
+      nil -> step
+      val -> move_on_list(%{map | position => val + 1}, position + val, step + 1)
+    end    
+  end
+
+  defp move_on_list2(map, position, step) do
+    case map[position] do
+      nil -> step
+      val when val >= 3 -> move_on_list2(%{map | position => val - 1}, position + val, step + 1)
+      val -> move_on_list2(%{map | position => val + 1}, position + val, step + 1)
+    end    
+  end
+  
+end
